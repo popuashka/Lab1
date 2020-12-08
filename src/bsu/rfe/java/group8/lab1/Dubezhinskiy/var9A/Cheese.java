db82bf85def31cd81903c941dde957dc94fd55a0
popuashka <dubezhinskiy96@mail.ru>
2020-12-08 17:20:18 +0300

    сделал

---
+++ src/bsu/rfe/java/group8/lab1/Dubezhinskiy/var9A/Cheese.java
@@ -0,0 +1,11 @@
+package bsu.rfe.java.group8.lab1.Dubezhinskiy.var9A;
+
+public class Cheese extends Food {
+    public Cheese() {
+        super("Cheese");
+    }
+
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+}
